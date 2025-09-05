ee94dd1717071f7854480876bcc9eb54a2ffa001
bl4ckf0xk <kavinduksks@gmail.com>
2025-09-05 16:38:34 +0530

    feat:new- git pages config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,12 +63,12 @@ const config: Config = {
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
         },
         slideUp: {
-          '0%': { transform: 'translateY(20px)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+          '0%': { transform: 'translateY(20px)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
         },
       },
     },
