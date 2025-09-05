5fea0bb2c1773dee2cffc25ffbb7b354308dab80
bl4ckf0xk <kavinduksks@gmail.com>
2025-09-05 14:44:22 +0530

    feat:new- git pages config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '', // Empty for root-level GitHub Pages
-  assetPrefix: '', // Ensure assets use relative paths
+  basePath: 'https://bl4ckf0xk.github.io/', // Empty for root-level GitHub Pages
+  assetPrefix: 'https://bl4ckf0xk.github.io/', // Ensure assets use relative paths
   trailingSlash: true, // Optional: Adds trailing slashes to help with routing
 };
 
