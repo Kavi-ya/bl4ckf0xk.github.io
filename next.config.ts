eb781ec23c0dac554c77e27300a9074ab12ea9ed
bl4ckf0xk <kavinduksks@gmail.com>
2025-09-05 14:27:40 +0530

    feat:new- git pages config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true }, // Disable image optimization for static export
-  basePath: process.env.NODE_ENV === 'production' ? '/bl4ckf0xk.github.io' : '',
+  basePath: '',
 };
 
 export default nextConfig;
